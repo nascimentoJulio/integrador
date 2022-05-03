d8718bde7b1a23da599b4169c0ee22abda8a5f51
NathanDoile <doile.souza@gmail.com>
2022-05-02 23:37:25 -0300

    Implementação de MetaDao, MetaDaoJDBC e TipoMeta

--- src/dao/MetaDao.java
+++ src/dao/MetaDao.java
@@ -1,5 +1,22 @@
 package dao;
 
-public interface MetaDao {
+import java.util.List;
 
+import models.Meta;
+import models.Usuario;
+
+public interface MetaDao {
+	
+	public void insert(Meta obj);
+	
+	public void update(Meta obj);
+	
+	public void deleteById(Integer id);
+	
+	public Meta findById(Integer id);
+	
+	public List<Meta> findAll();
+	
+	public List<Meta> findByUsuario(Usuario usuario);
+	
 }
---
+++ src/utils/enuns/TipoMeta.java
@@ -0,0 +1,5 @@
+package utils.enuns;
+
+public enum TipoMeta {
+
+}
